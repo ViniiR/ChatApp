bffabad456a3871d7d2637abfed810f1dd801648
ViniiR <cvrm2004@gmail.com>
2024-01-07 20:48:08 -0300

    initial commit 3

---
+++ src/Main.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM  from "react-dom/client";
+import App from "./App";
+
+const root = document.querySelector('.root');
+
+ReactDOM.createRoot(root!).render(
+    <React.StrictMode>
+        <App></App>
+    </React.StrictMode>
+)
